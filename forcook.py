31778059449961879f73f63690dc636a62295172
asila2104 <79084814+asila2104@users.noreply.github.com>
2021-04-11 20:59:13 +0300

    find + add ingredients(now no errors)

--- forcook.py
+++ forcook.py
@@ -33,31 +33,31 @@ class GoodGridLayout(GridLayout):
 class FindIngredientsButton(Button):
     pass
 
-class GoodPopup(Popup):
-    ref = ""
-    def set_ref(self, name):
-        self.ref = name
-    def get_ref(self):
-        return self.ref
-
 class GoodListGridLayout(GridLayout):
+    good_list = []
+
     def __init__(self, **args):
         super().__init__()
 
-    def add_good(self):
+    def add_good(self, text):
         gl = GoodGridLayout()
-        self.add_widget(gl)
+        tl = TestLabel(text = text, padding = [30, 0], color=(0, 0, 0, 1))
+        gl.add_widget(tl, 1)
+        self.add_widget(gl, 1)
+        self.good_list += [text]
+        print(self.good_list)
+
 
 class GoodTextInput(TextInput):
     def find_ingredients(self, name):
         result = []
         for it in session.query(Ingredients.name).filter(Ingredients.name.like(f'%{name}%')):
             result += [it]
-        return self.get_dict_list_from_result(result)
+        if result:
+            return self.get_dict_list_from_result(result)
 
     def print_find_ingredients(self, label):
         ingredients_list = self.find_ingredients(self.text.lower())
-
         label.clear_widgets()
         for i in ingredients_list:
             label.add_widget(FindIngredientsButton(text = i['name']))
@@ -68,6 +68,23 @@ class GoodTextInput(TextInput):
             i_dict = i._asdict()
             list_dict.append(i_dict)
         return list_dict
+
+class GoodPopup(Popup):
+    ref = ""
+    good_list_name = [object()]
+
+    def set_ref(self, name):
+        self.ref = name
+
+    def get_ref(self):
+        return self.ref
+
+    def good_list_id(self, name):
+        self.good_list_name += [name]
+
+    def good_list_id_get(self):
+        print("get", self.good_list_name)
+
 
 class DeleteFavButton(Button):
     pass
